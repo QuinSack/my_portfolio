8d5ac1747530986238af1f8f3fa8e3304474c3f3
Divine Sackitey <sackiteydivine24@gmail.com>
2023-11-03 22:42:46 +0000

    restructred experience section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -55,18 +55,59 @@ function Experience() {
 
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2022 - Present"
+          date="November 2022 - October 2023"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Back End Engineer - Petra Trust Company Limited
+            Backend Developer - Petra Trust Company Limited
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
             Dzorwulu
           </h4>
           <p>
-            Developed new features and maintained the backend infrastructure for in-house applications.
+            Developed a comprehensive mutual fund reporting service using .NET and ClosedXML library to generate and deliver daily reports with the aid of a cron to clients via mail, leveraging data from a financial API.
+          </p>
+          <p>
+            Provided maintenance support for 6+ .NET in-house applications. Achieved 100% stakeholder satisfaction and on-time completion.
+          </p>
+          <p>
+            Generated business reports upon request by stakolders using Postgresql and Metabase.
+          </p>
+          <p>
+            Collaborated with cross-functional teams, including frontend developers, business analysts and system administrators to ensure end-to-end functionality and resolve business challenges.
+          </p>
+          <p>
+            Actively participated in code reviews ensuring high code quality, adherence to coding standards, and knowledge sharing with the development team.
+          </p>
+          <p>
+            Demonstrated strong problem-solving skills through debugging technical issues and implementing effective error-handling mechanisms.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="April 2023 - Present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Frontend Developer - Jupeta Technologies
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+              Contract
+          </h4>
+          <p>
+            Assisted in the development of user interfaces for client projects using React.
+          </p>
+          <p>
+            Collaborated with team members to implement new features and enhance existing ones.
+          </p>
+          <p>
+            Participated actively in code reviews and provided constructive feedback.
+          </p>
+          <p>
+            Troubleshot and debugged technical issues.
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
