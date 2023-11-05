1aa184e25d1404617d5aa2023904959a7314f2b3
Divine Sackitey <sackiteydivine24@gmail.com>
2023-11-05 22:55:58 +0000

    refactor project list

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,8 +2,9 @@ import Proj1 from "../assets/proj1.png";
 import Proj2 from "../assets/proj2.png";
 import Proj3 from "../assets/proj3.png";
 import Proj4 from "../assets/proj4.png";
-import Proj5 from "../assets/proj5.jpg";
-import Proj6 from "../assets/proj6.png"
+import Proj5 from "../assets/proj5.png"
+
+
 
 export const ProjectList = [
   {
@@ -14,17 +15,24 @@ export const ProjectList = [
     demo: "http://quinsack-car-rentals.vercel.app"
   },
   {
-    name: "Thank You Small Library",
+    name: "Sneaker Shop",
     image: Proj2,
+    skills: "React,JavaScript,HTML,CSS",
+    link: "https://github.com/QuinSack/ecommerce_filtering",
+    demo: "https://ecommerce-filtering-blush.vercel.app/"
+  },
+  {
+    name: "Thank You Small Library",
+    image: Proj3,
     skills: "JavaScript,HTML,CSS,Firebase",
     link: "https://github.com/QuinSack/crud_library",
     demo: "https://crud-library.vercel.app"
   },
-  {
-    name: "Movie Site Clone",
-    image: Proj3,
-    skills: "React,HTML,CSS,OMDB API",
-  },
+  // {
+  //   name: "Movie Site Clone",
+  //   image: Proj3,
+  //   skills: "React,HTML,CSS,OMDB API",
+  // },
   {
     name: "Taskify",
     image: Proj4,
@@ -32,14 +40,14 @@ export const ProjectList = [
     link: "https://github.com/QuinSack/taskify",
     demo: "https://taskify-five-mu.vercel.app"
   },
-  {
-    name: "Dashboard Visualizer",
-    image: Proj5,
-    skills: "JavaScript,HTML,CSS",
-  },
+  // {
+  //   name: "Dashboard Visualizer",
+  //   image: Proj5,
+  //   skills: "JavaScript,HTML,CSS",
+  // },
   {
     name: "Cosmetics Hub",
-    image: Proj6,
+    image: Proj5,
     skills: "React,JavaScript,HTML,CSS",
     demo: "https://cosmetics-hub-qs52.vercel.app"
   }
